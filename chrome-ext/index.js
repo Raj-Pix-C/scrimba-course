cb842c8398444c3a166005853addcd9ddb520a12
raj <aam731555@gmail.com>
2026-08-07 17:09:23 +0600

    ggffgg

--- chrome-ext/index.js
+++ chrome-ext/index.js
@@ -2,14 +2,29 @@ let myLeads = new Set();
 let lead = "";
 
 const inputBtnEl = document.querySelector('#input-btn');
+const delBtnEl = document.querySelector('#del-btn');
 const inputEl = document.querySelector('#input-el');
 const ulEl = document.querySelector('#ul-el');
 
-// console.log(inputEl);
+
+
+
+function renderLeads(){
+    let s = ""
+    for(lead of myLeads){
+        s += `<li><a href="${lead}" target="_blank">${lead}</a></li>`;
+    }
+    ulEl.innerHTML += s;
+}
+
 
 inputBtnEl.addEventListener("click", () => {
-    if(!myLeads.has(lead) && lead !== ""){
+    if(!myLeads.has(lead) && lead !== "") {
+        // store leads
         myLeads.add(lead);
+        localStorage.setItem("myLeads", JSON.stringify(Array.from(myLeads)));
+            
+        // render new leads
         ulEl.innerHTML += `
             <li>
                 <a href="${lead}" target="_blank" >
@@ -17,20 +32,33 @@ inputBtnEl.addEventListener("click", () => {
                 </a>
             </li>`;
 
-        // // alt way //
-        // const li = document.createElement("li");
-        // li.textContent = lead;
-        // ulEl.append(li);
     }    
 
-    // for(let i = 0; i < myLeads.size; i++){
-    //     console.log(Array.from(myLeads)[i]);
-    // }
 
     inputEl.value = "";
 
 });
 
+
 inputEl.addEventListener("input", () => {
     lead = inputEl.value;
 });
+
+
+delBtnEl.addEventListener("dblclick", () => {
+    myLeads.clear();
+    localStorage.removeItem("myLeads");
+    ulEl.innerHTML = "";
+});
+
+
+// program procedural flow starts here
+
+
+
+const arr = JSON.parse(localStorage.getItem("myLeads"));
+
+if(arr) {
+    myLeads = new Set(arr);
+    renderLeads();
+}
