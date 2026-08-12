5d1aae916a33e10087c73f1a0e3341768d8498f6
raj <aam731555@gmail.com>
2026-08-12 18:38:56 +0600

    4

--- async-practice/index.js
+++ async-practice/index.js
@@ -1,22 +1,43 @@
-const request = new XMLHttpRequest();
-
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
-
-request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
-
-    if(request.readyState === 4 && request.status === 200) {
-        console.log(request, request.responseText);
-    }
-    else if(request.readyState === 4){
-        console.log('could not fetch data');
-    }
-})
-
 /**
  * readyState = 0 ==>> open("", "");
  * readyState = 1 ==>> send();
  * readyState = 2 ==>> downloading // partial
  * readyState = 3 ==>> done // full data if status == 200 // ok code
  */
+
+
+
+
+
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+
+    request.addEventListener('readystatechange', () => {
+
+        if(request.readyState === 4 && request.status === 200) {
+            callback(undefined, request.responseText);
+        }
+        else if(request.readyState === 4){
+            callback('could not fetch data', undefined);
+        }
+
+    });
+};
+
+
+
+
+
+
+getTodos( (err, data) => {
+    console.log('callback fired');
+    if(err){
+        console.log(err);
+    }
+    if(data){
+        console.log(data);
+    }
+});
