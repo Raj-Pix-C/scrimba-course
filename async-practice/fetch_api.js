575b645798fd0d1732aeeee4b1cdc00f45a86258
raj <aam731555@gmail.com>
2026-08-16 18:31:34 +0600

    10

--- async-practice/fetch_api.js
+++ async-practice/fetch_api.js
@@ -1,13 +1,29 @@
 
 
-fetch('todos/1.json')
-.then( (response) => {
-    console.log('resolved: ', response);
-    return response.json();
-})
-.then( (data) => {
-    console.log('data: ', data);
-})
-.catch( (err) => {
-    console.log('rejected: ', err);
-});
+// fetch('todos/1.json')
+// .then( (response) => {
+//     console.log('resolved: ', response);
+//     return response.json();
+// })
+// .then( (data) => {
+//     console.log('data: ', data);
+// })
+// .catch( (err) => {
+//     console.log('rejected: ', err);
+// });
+
+
+const getTodos = async (resource) => {
+    const response = await fetch(resource);
+    const data = await response.json();
+
+    return data;
+};
+
+getTodos('todos/1.json')
+    .then( (data) => {
+        console.log('resolved: ', data);
+    })
+    .catch( (err) => {
+        console.log('rejected: ', err);
+    });   
